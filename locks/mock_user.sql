b6793d00a4a80cf7e483fe43971bcb75030393fb
lyf <674619459@qq.com>
2023-01-11 15:58:07 +0800

    Initial commit

---
+++ locks/mock_user.sql
@@ -0,0 +1,3 @@
+use practice;
+# insert user(username, password) values('writelock','654321');
+# select * from user;
---
+++ locks/xiaoling_lock_practice.sql
@@ -0,0 +1,18 @@
+#
+use practice;
+# 插入数据
+# insert user(username, password) values('lyf','123456');
+# 全局锁
+# flush tables with read lock
+# unlock tables ;
+# 学习 数据库备份操作---
+
+# 表级锁
+# lock tables user read ;
+# lock tables user write ;
+# lock tables user write ;
+# unlock tables
+
+# 开启事务的使用
+# start transaction ;
+
